0b06d0b9d7ab9a8b97272b09696d37b509346700
Igor Aralin <exonymus@icloud.com>
2023-09-25 18:43:30 +0300

    Auth Pages [FrontEnd] [LGFM]

---
+++ public/js/auth.mjs
@@ -0,0 +1,23 @@
+$('.users-count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+
+        duration: 2500,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(addCommas(Math.ceil(now)));
+        }
+    });
+});
+function addCommas(nStr) {
+    nStr += '';
+    var x = nStr.split('.');
+    var x1 = x[0];
+    var x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+    return x1 + x2;
+}
